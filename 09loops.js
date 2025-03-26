038ff63c25f855bf20e340c2855c3e04f9077b0c
Kismat Chhetri <kismatchhetri23@gmail.com>
2025-03-26 21:07:25 +0545

    loop practice again

--- 09loops.js
+++ 09loops.js
@@ -1,34 +1,34 @@
-console.log("loop in javascript");
+// console.log("loop in javascript");
 
-console.log( "for loop");
+// console.log( "for loop");
 
-let sum = 0;
-let n = 4;
-for(let i=0; i<n ;i++)
-{
-    sum += (i+1);
-    console.log((i+1) , "+");
+// let sum = 0;
+// let n = 4;
+// for(let i=0; i<n ;i++)
+// {
+//     sum += (i+1);
+//     console.log((i+1) , "+");
 
-}
-console.log( "sum of first natural numbers",n,"numbers" , sum,);
+// }
+// console.log( "sum of first natural numbers",n,"numbers" , sum,);
 
-eg
-
-
-for in loop
+// eg
 
 
-let game={
-    fight:"200",
-    price:500,
-    buy:"now"
+// for in loop
 
-}
-for(let a in game){
-    // console.log("marks of ",a ,"are" ,game[a]);
-    // console.log(a);
-    console.log(game[a]);
-}
+
+// let game={
+//     fight:"200",
+//     price:500,
+//     buy:"now"
+
+// }
+// for(let a in game){
+//     // console.log("marks of ",a ,"are" ,game[a]);
+//     // console.log(a);
+//     console.log(game[a]);
+// }
 
 
 //for of loop
@@ -36,4 +36,60 @@ for(let a in game){
 // for(let b of "kismat"){
 //     console.log(b);
 // }
+
+
+
+
+
+
+// AGAIN
+
+// for(let i=0;i<5;i++)
+// {
+//     console.log(i+1);
+// }
+
+// let n =prompt("enter the value of n");
+// n=Number.parseInt(n);
+// let sum=0;
+// for(let i=0;i<5;i++)
+// {
+//     sum+=(i+1);  
+//     console.log((i+1),"+");
+// }
+// console.log("sum of first ","natural number is " ,sum );
+
+
+// let k=1;
+// for(let i=0;i<5;i++)
+// {
+//     k=k*(i+1);
+
+// }
+// console.log(k);
+
+
+
+let k ={
+ abhimat:20,
+ kismat:10,
+ neon:14
+
+}
+console.log(k ["abhimat"]);
+
+for(a in k){
+    console.log("marks of ",a,"are",k[a]);
+}
+
+
+for(a of "abhimat"){
+    console.log(a);
+}
+
+
+
+
+//for of loop
+
 
--- 10whileloop.js
+++ 10whileloop.js
@@ -1,20 +1,37 @@
-console.log("while loop in javascript");
+// console.log("while loop in javascript");
 
-let n = prompt("enter the value of n");
-n = Number.parseInt(n);
+// let n = prompt("enter the value of n");
+// n = Number.parseInt(n);
 
-let i=0;
+// let i=0;
 
-while(i<n){
+// while(i<n){
+//     console.log(i);
+//     i++;
+// }
+
+
+// //  do while loop
+// let k =0;
+// do{
+//     console.log(k);
+//     k++;
+// }
+// while(k<=5);
+
+
+
+//AGAIN
+
+// let i = 8;
+// while(i<6){
+//     console.log(i);
+//     i++;
+// }
+
+let i = 0;
+do{
     console.log(i);
     i++;
 }
-
-
-//  do while loop
-let k =0;
-do{
-    console.log(k);
-    k++;
-}
-while(k<=5);
+while(i<5);
